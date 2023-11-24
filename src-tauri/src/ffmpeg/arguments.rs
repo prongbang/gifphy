97b74208621eee6278b01a6757d66a2b1fc87b2b
prongbang <prongbang@gmail.com>
2023-11-24 19:40:15 +0700

    fixbug: Force overwrite

--- src-tauri/src/ffmpeg/arguments.rs
+++ src-tauri/src/ffmpeg/arguments.rs
@@ -16,9 +16,10 @@ pub fn gen_palette(option: &options::Options) -> Vec<String> {
 }
 
 pub fn use_palette(option: &options::Options) -> Vec<String> {
-    let fc = format!("fps={},scale={}:-1:flags={}", &option.fps, &option.scale, &option.flags_gen);
+    let fc = format!("fps={},scale={}:-1:flags={}", &option.fps, &option.scale, &option.flags_use);
 
     let args = vec![
+        "-y".to_string(), // Force overwrite
         "-ss".to_string(), option.start_at_second.clone(),
         "-t".to_string(), option.length_of_gif_video.clone(),
         "-i".to_string(), option.input_file.clone(),
---
+++ src/utils/status.ts
@@ -0,0 +1,3 @@
+export const IN_PROGRESS_STATUS = "in-progress"
+export const DONE_STATUS = "done"
+export const ERROR_STATUS = "error"
